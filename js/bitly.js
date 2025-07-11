5c12b18656879e748cc77da526a0aab34ea7078e
Gourav Narvariya <gourav.narvariya@vkaps.com>
2025-07-11 20:52:04 +0530

    feature added silverLining

--- js/bitly.js
+++ js/bitly.js
@@ -1,25 +1,37 @@
-/* global process */
-require('dotenv').config()
-const axios = require('axios')
-const accessToken = process.env.API_BITLY
-const bitlyApiUrl = 'https://api-ssl.bitly.com/v4/shorten'
+require('dotenv').config();
+const axios = require('axios');
 
-const createShortBitly = async longUrl => {
-  const response = await axios.post(
-    bitlyApiUrl,
-    {
-      long_url: longUrl,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
+const apiKey = process.env.API_SILVERLINING;
+const silverLiningApiUrl = 'https://api.aws3.link/shorten';
+const domains = process.env.CUSTOM_DOMAIN.split(',').map(domain => domain.trim());
+let domainIndex = 0;
+
+const createShortUrl = async (longUrl) => {
+  try {
+    const selectedDomain = domains[domainIndex % domains.length];
+    domainIndex++;
+
+    const response = await axios.post(
+      silverLiningApiUrl,
+      {
+        longUrl: longUrl,
+        slugLength: 8,
+        customDomain: selectedDomain,
       },
-    },
-  )
+      {
+        headers: {
+          'x-api-key': apiKey,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
-  const shortUrl = response.data.id
-  return shortUrl
-}
+    return response.data.shortUrl;
 
-module.exports = createShortBitly
+  } catch (error) {
+    console.error('Error shortening URL:', error.response?.data || error.message);
+    throw error;
+  }
+};
+
+module.exports = createShortUrl;
