1389e7e1a589e1995629a1c5d515b3af8d836e65
gouravN0709 <gourav.narvariya@vkaps.com>
2025-07-11 22:01:52 +0530

    Feature Custom domain Updated

--- js/bitly.js
+++ js/bitly.js
@@ -4,29 +4,30 @@ const axios = require('axios');
 const apiKey = process.env.API_SILVERLINING;
 const silverLiningApiUrl = 'https://api.aws3.link/shorten';
 const domains = process.env.CUSTOM_DOMAIN.split(',').map(domain => domain.trim());
-let domainIndex = 0;
 
 const createShortUrl = async (longUrl) => {
   try {
-    const selectedDomain = domains[domainIndex % domains.length];
-    domainIndex++;
-
-    const response = await axios.post(
-      silverLiningApiUrl,
-      {
-        longUrl: longUrl,
-        slugLength: 8,
-        customDomain: selectedDomain,
-      },
-      {
-        headers: {
-          'x-api-key': apiKey,
-          'Content-Type': 'application/json',
-        },
-      }
+    const shortUrls = await Promise.all(
+      domains.map(async (selectedDomain) => {
+        const response = await axios.post(
+          silverLiningApiUrl,
+          {
+            longUrl: longUrl,
+            slugLength: 8,
+            customDomain: selectedDomain,
+          },
+          {
+            headers: {
+              'x-api-key': apiKey,
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        return response.data.shortUrl;
+      })
     );
 
-    return response.data.shortUrl;
+    return shortUrls;
 
   } catch (error) {
     console.error('Error shortening URL:', error.response?.data || error.message);
@@ -35,3 +36,4 @@ const createShortUrl = async (longUrl) => {
 };
 
 module.exports = createShortUrl;
+
